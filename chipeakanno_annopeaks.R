b81bbf4662bc9f8a936ccdf5813c3cd8020c579d
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2018-05-02 13:16:21 +0000

    first commit

---
+++ chipeakanno_annopeaks.R
@@ -0,0 +1,23 @@
+require("ChIPpeakAnno", quietly = TRUE)
+require("EnsDb.Hsapiens.v75", quietly = TRUE)
+require("rtracklayer", quietly = TRUE)
+
+options(warn = -1)
+options("download.file.method"="wget")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+input1 = args[1]
+input2 = args[2]
+input3  = as.numeric(args[3])
+input4 = as.numeric(args[4])
+output1  = args[5]
+
+peaks <- get(load(input1))
+
+annoData <- toGRanges(EnsDb.Hsapiens.v75)
+seqlevelsStyle(peaks) <- seqlevelsStyle(annoData)
+
+ anno <- annoPeaks(peaks, annoData=annoData, bindingType =c(input2), bindingRegion=c(input3, input4))
+ 
+ write.table(anno, file= output1, row.names = FALSE)
